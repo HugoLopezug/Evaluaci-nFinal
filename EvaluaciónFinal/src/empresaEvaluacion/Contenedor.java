a8f20702733be348e68583dffbc5e9dbd1f373a4
ghost <ghost@DESKTOP-T0KOTE5>
2021-02-05 20:46:19 -0300

    Agregar Lista Capacitación en contenedor. Agregar constructor lleno en clase contenedor. Con profe josé

--- EvaluaciónFinal/src/empresaEvaluacion/Contenedor.java
+++ EvaluaciónFinal/src/empresaEvaluacion/Contenedor.java
@@ -5,23 +5,51 @@ import java.util.List;
 
 public class Contenedor implements Asesoria {
 
-	private List<Asesoria> lista;
+	
+	private List<Asesoria> listaAsesoria;
+	private List<Capacitacion> listaCapacitacion;
 
+	
+	
 	public Contenedor() {
-		lista = new ArrayList<Asesoria>();
+		listaAsesoria = new ArrayList<Asesoria>();
+		listaCapacitacion  = new ArrayList<Capacitacion>();
+	}
+
+	
+	public Contenedor(List<Asesoria> listaAsesoria, List<Capacitacion> listaCapacitacion) {
+		super();
+		this.listaAsesoria = listaAsesoria;
+		this.listaCapacitacion = listaCapacitacion;
+		
+	}
+
+	public List<Asesoria> getListaAsesoria() {
+		return listaAsesoria;
+	}
+
+	public void setListaAsesoria(List<Asesoria> listaAsesoria) {
+		this.listaAsesoria = listaAsesoria;
+	}
+
+	public List<Capacitacion> getListaCapacitacion() {
+		return listaCapacitacion;
+	}
+
+	public void setListaCapacitación(List<Capacitacion> listaCapacitacion) {
+		this.listaCapacitacion = listaCapacitacion;
 	}
 
 	public void agregarUsuario(Asesoria usuario) {
 
-		lista.add(usuario);
+		listaAsesoria.add(usuario);
 
 	}
 
 	public void analizarUsuario() {
-		for (Asesoria auxUsuario : lista) {
+		for (Asesoria auxUsuario : listaAsesoria) {
 			auxUsuario.analizarUsuario();
 			System.out.println("");
 		}
 	}
- // C
 }
--- EvaluaciónFinal/src/empresaEvaluacion/main.java
+++ EvaluaciónFinal/src/empresaEvaluacion/main.java
@@ -1,16 +1,31 @@
 package empresaEvaluacion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class main {
 
 	public static void main(String[] args) {
 
+		List<Asesoria> listaAsesoria = new ArrayList<Asesoria>();
+		List<Capacitacion> listaCapacitacion  = new ArrayList<Capacitacion>();
+		
+		
 		Contenedor sc = new Contenedor();
+		
+		Contenedor sc2= new Contenedor(listaAsesoria, listaCapacitacion);
+		
 		Profesional prof1 = new Profesional("Daniel", "18/02/1990", "17257626-5", "Ingeniera ambiental", "14/01/2021");
 		Profesional prof2 = new Profesional("Andres", "13/01/1987", "18789568-7", "Ingeniera agronomo", "11/01/2019");
 		Profesional prof3 = new Profesional("Francisca", "20/08/1992", "18457985-8", "Profesora Ingles", "11/09/2018");
 		Profesional admin1 = new Profesional("Ricardo", "18/02/1984", "14578123-6", "Logistica", "8 años");
 		Administrativo admin2 = new Administrativo("Julio", "14/09/1985", "16547985-9", "Recursos Humnanos", "5 años");
 
+		
+		
+		listaAsesoria.add(admin2);
+		
+		
 		sc.agregarUsuario(prof1);
 		sc.agregarUsuario(prof2);
 		sc.agregarUsuario(prof3);
@@ -19,7 +34,10 @@ public class main {
 
 		sc.analizarUsuario();
 
-		// Q
+		System.out.println(sc.getListaAsesoria().size());
+		System.out.println(sc2.getListaAsesoria().size());
+	
+		
 	}
 
 }
