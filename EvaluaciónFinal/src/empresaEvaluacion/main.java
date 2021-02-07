bc04dd50c301b45eae6afde4d05e7e04dad376d4
ghost <ghost@DESKTOP-T0KOTE5>
2021-02-07 20:33:15 -0300

    Agrego Espacio y cambio el < de rut y run por >

--- EvaluaciónFinal/src/empresaEvaluacion/main.java
+++ EvaluaciónFinal/src/empresaEvaluacion/main.java
@@ -74,11 +74,12 @@ public class main {
 				case 1:
 
 					Cliente cliente1 = new Cliente();
-
+					System.out.println("             ");
 					System.out.println("1. Ingresando perfil de Cliente (min 10 caracteres, max 50)");
 					
 					// Almacenar Nombre de Usuario
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese nombre de usuario");
 						nombreUsuario = leer.nextLine().strip();
 					} while (nombreUsuario.length() < 10 || nombreUsuario.length() > 50);
@@ -86,6 +87,7 @@ public class main {
 					
 					// Almacenar fecha de nacimiento de Usuario
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese fecha de nacimiento de usuario (formato dd/mm/aa)");
 						fechaNacimiento = leer.nextLine();
 						validadorFecha = validarFecha(fechaNacimiento);
@@ -93,21 +95,24 @@ public class main {
 					
 					// Almacenar Run Usuario
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese RUN de usuario sin digito verificador (hasta 8 digitos)");
 						run = validarEnteros();
-					} while (run == null || run < 99999999);
+					} while (run == null || run > 99999999);
 					cliente1.setRun(run);
 					
 					// Almacenar Rut Cliente
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese RUT de cliente sin digito verificador (hasta 8 digitos)");
 						rut = validarEnteros();
-					} while (rut == null || rut < 99999999);
+					} while (rut == null || rut > 99999999);
 					cliente1.setRut(rut);
 					
 					// Almacenar Nombres Cliente
 					leer.nextLine();
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese nombres de cliente (min 5 caracteres, max 30)");
 						nombresCliente = leer.nextLine().strip();
 					} while (nombresCliente.length() < 5 || nombresCliente.length() > 30);
@@ -115,6 +120,7 @@ public class main {
 					
 					// Almacenar Apellidos Cliente
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese apellidos de cliente (min 5 caracteres, max 30)");
 						apellidosCliente = leer.nextLine().strip();
 					} while (apellidosCliente.length() < 5 || apellidosCliente.length() > 30);
@@ -122,12 +128,14 @@ public class main {
 					
 					// Almacenar Telefono Cliente
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese telefono de cliente (8 digitos) ");
 						telefono = leer.nextLine().strip();
 					} while (telefono.length() != 8 || !telefono.matches("[0-9]*"));
 					
 					// Almacenar AFP Cliente
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese AFP de cliente (min 4 caracteres, max 30)");
 						afp = leer.nextLine().strip();
 					} while (afp.length() < 4 || afp.length() > 30);
@@ -135,6 +143,7 @@ public class main {
 					
 					// Almacenar Sistema de Salud Cliente
 					do {
+						System.out.println("             ");
 						System.out.println("Sistema de salud de cliente, Ingrese:");
 						System.out.println("1 para Fonasa ");
 						System.out.println("2 para Isapre ");
@@ -144,6 +153,7 @@ public class main {
 					
 					// Almacenar Direccion Cliente
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese direccion de cliente (maximo 70 caracteres)");
 						direccion = leer.nextLine().strip();
 					} while (direccion.length() > 70 || direccion == "");
@@ -158,6 +168,7 @@ public class main {
 					
 					// Almacenar Edad Cliente
 					do {
+						System.out.println("             ");
 						System.out.println("Ingrese edad de cliente (rango de edad 0-150 años)" + "\n");
 						edad = validarEnteros();
 					} while (edad == null || edad < 0 || edad > 150);
@@ -175,6 +186,7 @@ public class main {
 
 					Profesional profesional1 = new Profesional();
 
+					System.out.println("             ");
 					System.out.println("Ingresando perfil profesional");
 					do {
 						System.out.println("Ingrese nombre del profesional");
